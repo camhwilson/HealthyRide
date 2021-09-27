af1a390b9d2e6d5da89436a990e029f78bf93b43
Cameron Wilson <camhwilson@me.com>
2021-09-26 20:10:48 -0400

    added ability to track departures and arrivals from adjacent neighborhoods

--- trip.py
+++ trip.py
@@ -2,6 +2,31 @@ from dateutil import parser
 from dataclasses import dataclass
 
 
+station_dict = {
+    'Downtown': ['1000', '49701', '1002', '1001', '49691', '49671', '1056', '1003', 
+                                '49731', '1004', '1006', '49651', '1010'],
+    
+    'Strip District': ['1016', '1017', '49611', '1059', '1060'],
+
+    'East Liberty' : ['1026', '1027', '1029', '1024', '49391', '49371', '1064'],
+
+    'Lawrenceville' : ['1061', '49581', '1019', '1020', '49501', '49951', '49561', '1018'],
+
+    'North Shore' : ['49941', '49921', '1012', '1013'],
+
+    'Central Northside' : ['1014', '1015', '49881'],
+
+    'Southside Flats' : ['1074', '1049', '1048', '1047', '1046', '1045', '1084'],
+
+    'Oakland' : ['1038', '1044', '1039', '1091', '1093', '1041', '1095', '1036', '1037', '1099', 
+                        '1040', '1097', '1035', '1094', '49271'],
+    
+    'Squirrel Hill' : ['49251', '49261', '1068', '1069', '1070'],
+    
+    'Shadyside': ['1034', '1033', '1032', '1031'] 
+
+}
+
 @dataclass
 class Trip:
     """This is a dataclass that """
@@ -23,6 +48,9 @@ class Trip:
         that function's documentation for an explanation of functionality.
         '''
         self.weekday = self.datestr_to_dayint(self.starttime)
+        self.start_neighborhood = self.determine_neighborhood(self.fromid)
+        self.end_neighborhood = self.determine_neighborhood(self.toid)
+
     
     def weekday_arrivals_todict(list_of_trip_objects):
         '''
@@ -98,3 +126,11 @@ class Trip:
             return datetime_obj.weekday()
         except:
             return 'missing date'
+
+    def determine_neighborhood(self, search_param):
+        for key, val in station_dict.items():
+            if search_param in val:
+                return key
+        return 'Not a station in a defined neighborhood'
+
+
