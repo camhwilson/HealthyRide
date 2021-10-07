b053cd44e5dfcb2b00125b81684a2c6b1dacf23f
Cameron Wilson <camhwilson@me.com>
2021-10-07 19:23:32 -0400

    for chris to check out

--- neighborhood_analytics/hour.py
+++ neighborhood_analytics/hour.py
@@ -6,12 +6,15 @@ class Hour:
         """function groups trip lists by hour of day"""
         #print(len(filter_list_to_just_objects(Trip_list)[1]))
         hourly_trip_dict = {}
-        for i in Trip_list:
-            date = i.starttime
+        trip_list_wo_dockless = self.drop_dockless_arrivals(Trip_list)
+        
+        for i in trip_list_wo_dockless:
+            date = i.stoptime
             try:
                 hour = date[date.find(' ')+len(' '):date.rfind(':')]
             except:
-                hour = 'missing date'
+                #“dockless” trips are where a bike does not start or end at an official station, per healthyride documentation
+                hour = 'dockless'
 
             if hour not in hourly_trip_dict.keys():
                 hourly_trip_dict[hour] = [i.end_neighborhood]
@@ -24,12 +27,14 @@ class Hour:
     def group_trip_list_by_departure_hour(self, Trip_list):
         """function groups trip lists by hour of day"""
         hourly_trip_dict = {}
-        for i in Trip_list:
-            date = i.stoptime
+        trip_list_wo_dockless = self.drop_dockless_departures(Trip_list)
+        for i in trip_list_wo_dockless:
+            date = i.starttime
             try:
                 hour = date[date.find(' ')+len(' '):date.rfind(':')]
             except:
-                hour = 'missing date'
+                #“dockless” trips are where a bike does not start or end at an official station, per healthyride documentation
+                hour = 'dockless'
             
             if hour not in hourly_trip_dict.keys():
                 hourly_trip_dict[hour] = [i.start_neighborhood]
@@ -43,3 +48,11 @@ class Hour:
         for key, val in hourly_trip_dict.items():
             counted_hourly_neighborhoods[key] = Counter(val)
         return counted_hourly_neighborhoods
+
+    @classmethod
+    def drop_dockless_departures(self, Trip_list):
+        return [i for i in Trip_list if type(i.starttime) is not None]
+
+    @classmethod
+    def drop_dockless_arrivals(self, Trip_list):
+        return [i for i in Trip_list if type(i.stoptime) is not None]
