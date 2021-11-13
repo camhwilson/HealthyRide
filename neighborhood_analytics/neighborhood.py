277880f421f62f8ba67a2f1b461c77e8f20038f6
Cameron Wilson <camhwilson@me.com>
2021-11-13 12:20:31 -0500

    consolidated all visuals to analytics.ipynb

--- neighborhood_analytics/neighborhood.py
+++ neighborhood_analytics/neighborhood.py
@@ -1,3 +1,7 @@
+import sys
+
+sys.path.insert(0, '/Users/cwilson/desktop/programming/jupyter_tings/healthyride/healthyride/neighborhood_analytics')
+
 from hour import Hour
 from weekday import Weekday
 
